5f640d86179b18bd430e1fb8c944e0f97cb75e14
Marcus Vorwaller <marcus@vorwaller.net>
2014-10-20 10:35:35 -0700

    Update anki.rb to 2.0.31
    The previous version is no longer available for download.

--- Casks/anki.rb
+++ Casks/anki.rb
@@ -1,5 +1,5 @@
 class Anki < Cask
-  version '2.0.29'
+  version '2.0.31'
   sha256 '2eea38eca7e4f720c01a10ffdbfe0b73c9ac06648483103ed034f9db6660c07a'
 
   url "http://ankisrs.net/download/mirror/anki-#{version}.dmg"
